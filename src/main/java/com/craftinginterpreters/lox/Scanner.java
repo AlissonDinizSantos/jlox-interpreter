5d8a04d695701f11e1b7594b83f46a8e802c1a5d
AlissonDinizSantos <alissondinizsantos@gmail.com>
2025-12-15 11:22:06 -0300

    feat: implement scanner state and single-character tokens

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -3,17 +3,63 @@ package com.craftinginterpreters.lox;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.craftinginterpreters.lox.TokenType.*;
+
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
   Scanner(String source) {
     this.source = source;
   }
 
   List<Token> scanTokens() {
-    // Por enquanto retorna apenas o token de fim de arquivo
-    tokens.add(new Token(TokenType.EOF, "", null, 0)); // Linha fictícia 0
+    while (!isAtEnd()) {
+      // Estamos no início do próximo lexema.
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
     return tokens;
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+      case '(': addToken(LEFT_PAREN); break;
+      case ')': addToken(RIGHT_PAREN); break;
+      case '{': addToken(LEFT_BRACE); break;
+      case '}': addToken(RIGHT_BRACE); break;
+      case ',': addToken(COMMA); break;
+      case '.': addToken(DOT); break;
+      case '-': addToken(MINUS); break;
+      case '+': addToken(PLUS); break;
+      case ';': addToken(SEMICOLON); break;
+      case '*': addToken(STAR); break; 
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
+    }
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
   }
 }
